62adcf5a50e66f4d548d589c81f4ea6b0479c5cf
Michael Sackstein <m_sackstein@yahoo.com>
2020-04-22 15:38:35 -0400

    Done

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,22 @@
 # your code goes here
 
+require 'pry'
 class Person
 
     attr_reader :name, :happiness, :hygiene
     attr_accessor :bank_account
+    @@all = []
     
     def initialize(name)
         @name = name
         @bank_account = 25
         @happiness = 8
         @hygiene = 8
+        @@all << self
+    end
+
+    def self.all
+        @@all
     end
 
     def happiness=(happy)
@@ -33,19 +40,48 @@ class Person
     end
 
     def clean?
-        if @hygiene > 7
-            true
-        else
-            false
-        end
+        @hygiene > 7 ? true : false
     end
 
     def happy?
-        if @happiness > 7
-            true
-        else
-            false
-        end
+        @happiness > 7 ? true : false
     end
 
+    def get_paid(salary_amount)
+        # binding.pry
+        self.bank_account += salary_amount
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        # binding.pry
+        friend.happiness += 3
+        self.happiness += 3
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
 end
