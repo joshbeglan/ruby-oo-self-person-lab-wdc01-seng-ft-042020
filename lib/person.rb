e258f150809d7afad7c5612dffc563e7fd2e7d45
github username <github email address>
2020-04-22 14:29:23 -0400

    happy?

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,51 @@
 # your code goes here
+
+class Person
+
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+    
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(happy)
+        if happy > 10
+            @happiness = 10
+        elsif happy < 0
+            @happiness = 0
+        else
+            @happiness = happy
+        end
+    end
+
+    def hygiene=(hygiene)
+        if hygiene > 10
+            @hygiene = 10
+        elsif hygiene < 0
+            @hygiene = 0
+        else
+            @hygiene = hygiene
+        end
+    end
+
+    def clean?
+        if @hygiene > 7
+            true
+        else
+            false
+        end
+    end
+
+    def happy?
+        if @happiness > 7
+            true
+        else
+            false
+        end
+    end
+
+end
